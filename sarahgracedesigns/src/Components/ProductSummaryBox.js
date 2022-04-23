8beaf89b502727596d338525b94b30325b1211f0
sarahtan931 <sarah.tan931@gmail.com>
2022-04-22 21:37:04 -0400

    adding checkout frontend page

---
+++ sarahgracedesigns/src/Components/ProductSummaryBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Styles/Checkout.scss';
+
+export default function ProductSummaryBox(props) {
+  const product = props.product;
+  return (
+    <div className='productsummary'>
+      <div className='productsummary__left'>
+        <div className='productsummary__imagebox'>
+          <div className='productsummary__quantity'>1</div>
+          <img src={product.productimgurl} alt="" className='productsummary__img'></img>
+        </div>
+        <div className='productsummary__title'>{product.title}</div>
+      </div>
+      <div className='productsummary__price'>${product.price}.00</div>
+    </div>
+  )
+}
--- sarahgracedesigns/src/Pages/Checkout.js
+++ sarahgracedesigns/src/Pages/Checkout.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import '../Styles/Checkout.scss';
 import axios from 'axios';
 import configdata from '../config.json';
-
+import CheckoutSummary from '../Components/CheckoutSummary';
+import { useLocation } from 'react-router-dom';
 
 export default function Checkout() {
+    const [products, setProducts] = useState([]);
+    const [subtotal, setSubtotal] = useState(0);
+    const email = localStorage.getItem('email');
     const stripe = useStripe();
     const elements = useElements();
     const url = configdata.url;
+    const location = useLocation();
     const cardElementOptions = {
         style: {
             base: {
-                fontSize: '24px',
+                fontSize: '16px',
                 color: 'black',
-                iconColor: 'white',
+                iconColor: 'grey',
                 "::placeholder": {
-                    color: 'white'
+                    color: 'grey'
                 }
             },
             invalid: {
@@ -29,7 +34,69 @@ export default function Checkout() {
         hidePostalCode: true
     }
 
-    const handleFormSubmit = async() => {
+
+    //when initiating cart, if user authenticated get values from db else get values from local storage
+    useEffect(() => {
+        if (location.state.products != null && location.state.subtotal != null) {
+            setProducts(location.state.products);
+            setSubtotal(location.state.subtotal);
+        } else {
+            const isAuth = localStorage.getItem('isAuth');
+            if (isAuth) {
+                GetUserCart();
+            } else {
+                GetLocalCart();
+                Products();
+            }
+        }
+    }, []);
+
+    //helper function to get products from local storage
+    const GetLocalCart = () => {
+        let cartItemString = localStorage.getItem('cartItemIds');
+        let cartItemArr = JSON.parse(cartItemString);
+        let price = 0;
+        let productIdArr = [];
+        if (cartItemArr != null && cartItemArr.length > 0) {
+            cartItemArr.forEach(cartItem => {
+                productIdArr.push(cartItem.productid)
+            })
+
+            let idArr = productIdArr.join(',');
+            axios.get(`${url}/api/products/ids/${idArr}`).then((response) => {
+                let productTemp = response.data;
+                productTemp.forEach((product, index) => {
+                    let cartItem = cartItemArr.find(x => x.productid == product.productid);
+                    price += (parseInt(product.price) * cartItem?.quantity);
+                    productTemp[index].cartitemquantity = cartItem.quantity;
+                });
+
+                setProducts([...productTemp]);
+                setSubtotal(price);
+            });
+        } else {
+            setProducts([]);
+        }
+    }
+
+    //helper function to get products from db
+    const GetUserCart = () => {
+        axios.get(`${url}/api/cart/${email}`).then((response) => {
+            const sorted = response.data?.sort((a, b) => a.id - b.id);
+            setProducts([...sorted]);
+            let price = 0;
+            response.data?.forEach(item => {
+                price += (item.cartitemquantity * item.price)
+            });
+            setSubtotal(price);
+
+        }).catch((err) => {
+            console.log(err)
+        });
+    }
+
+
+    const handleFormSubmit = async () => {
         //getting card element
         const cardElement = elements.getElement(CardElement);
 
@@ -53,7 +120,7 @@ export default function Checkout() {
         })
 
         //getting payment confirmation
-        axios.post(`${url}/api/payment_intents`, { amount: 123 }).then(async(response) => {
+        axios.post(`${url}/api/payment_intents`, { amount: 123 }).then(async (response) => {
             const clientSecret = response.data;
             const confirmCardPayment = await stripe.confirmCardPayment(clientSecret, {
                 payment_method: paymentMethodReq.paymentMethod.id
@@ -67,20 +134,44 @@ export default function Checkout() {
         });
 
     }
-   
+
+
+
     return (
         <div>
+            <div className='checkoutback'>Back to Cart</div>
+            <div className="checkoutlogo">
+                <img src="https://sarahgracedesignsbucket.s3.amazonaws.com/SarahGraceLogo.png" alt="Sarah Grace Designs Header Image" />
+            </div>
             <div className='checkout'>
                 <div className="checkout__info">
-
-                    <div className="checkout__cardbox">
-                        <CardElement options={cardElementOptions}></CardElement>
+                    <div className="checkout__infobox">
+                        <div className="checkout__contactbox">
+                            <p className="checkout__subtitle">Contact Information</p>
+                            <input className="checkout__input" type="title" placeholder='Name' name="name" ></input>
+                            <input className="checkout__input" type="title" placeholder='Email' name="email" ></input>
+                        </div>
+                        <div className="checkout__shipping">
+                            <p className="checkout__subtitle">Shipping Information</p>
+                            <input className="checkout__input" type="title" placeholder='Address' name="address" ></input>
+                            <input className="checkout__input" type="title" placeholder='City' name="city" ></input>
+                            <div className='checkout__flexinput'>
+                                <input className="checkout__input" type="title" placeholder='Country' name="country" ></input>
+                                <input className="checkout__input" type="title" placeholder='State' name="state" ></input>
+                            </div>
+                            <input className="checkout__input" type="title" placeholder='Postal Code' name="postalcode" ></input>
+                        </div>
+                        <div className="checkout__payment">
+                            <p className="checkout__subtitle">Payment Information</p>
+                            <div className="checkout__cardbox">
+                                <CardElement options={cardElementOptions}></CardElement>
+                            </div>
+                        </div>
                     </div>
-                    <button onClick={handleFormSubmit}>submit</button>
-
                 </div>
                 <div className="checkout__review">
-                    review
+                    <CheckoutSummary subtotal={subtotal} products={products}></CheckoutSummary>
+                        <button onClick={handleFormSubmit} className="checkoutbutton">Order Now</button>
                 </div>
             </div>
 
